c4b3ec68f5092e69268e915c35d95d0659638231
ajay123-k <ajaykhandare862@gmail.com>
2022-10-08 13:10:50 +0530

    This is a basic quiz app of html css and js
    This is a basci quiz with 30 question of html css and js , where you can check your basic knowledge of html css and js.

---
+++ quiz.js
@@ -0,0 +1,426 @@
+let questions = [
+    
+    {
+
+    'que' : 'Which of the following tags doesn’t require a closing tag',
+    'a': '<br>',
+    'b': '<hr>',
+    'c': 'Both ',
+    'd': 'None of the above',
+    'correct' : 'c'
+
+},
+    {
+
+    'que' : 'How many sizes of headers are available in the HTML by default',
+    'a': '5',
+    'b': '4',
+    'c': '1',
+    'd': '6',
+    'correct' : 'd'
+
+},
+
+{
+    'que' : 'Which is the smallest header in HTML by default?',
+    'a' :'h1',
+    'b':'h2',
+    'c':'h4',
+    'd':'h6',
+    'correct' :'d'
+},
+{
+    'que' : 'Which attribute is used to provide a unique name to an HTML element?',
+    'a' :'class',
+    'b':'id',
+    'c':'type',
+    'd':'name',
+    'correct' :'b'
+},
+{
+    'que' : 'Which tag used to render an image on a webpage?',
+    'a' :'img',
+    'b':'image',
+    'c':'scr',
+    'd':'picture',
+    'correct' :'a'
+},
+{
+    'que' : 'How the black color is represented in terms of RGB values?',
+    'a' :'RGB(100,0,100)',
+    'b':'RGB(0,100,0)',
+    'c':'RGB(0,0,0)',
+    'd':'RGB(100,100,100)',
+    'correct' :'c'
+},
+{
+    'que' : 'Which of the following is block-level element in HTML?',
+    'a' :'<div>',
+    'b':'<p>',
+    'c':'<h1>',
+    'd':'All of the above',
+    'correct' :'d'
+},
+    {
+
+    'que' : 'What are the types of lists are available in the HTML?',
+    'a': 'Ordered , Unordered Lists.',
+    'b': 'Bullet , Numbered Lists',
+    'c': 'Named , Unnamed Lists',
+    'd': 'None of the above',
+    'correct' : 'a'
+
+},
+    {
+
+    'que' : 'HTML files are saved by default with the extension?',
+    'a': '.html',
+    'b': '.h',
+    'c': '.ht',
+    'd': 'None of the above',
+    'correct' : 'a'
+
+},
+    {
+
+    'que' : 'We close HTML tags within?',
+    'a': '{ }',
+    'b': '< >',
+    'c': '! !',
+    'd': '( )',
+    'correct' : 'b'
+
+},
+    {
+
+    'que' : 'The full form of CSS is',
+    'a': 'Coloured Special Sheets',
+    'b': 'Cascading Style Sheets',
+    'c': 'Color and Style Sheets',
+    'd': 'None of the above',
+    'correct' : 'b'
+
+},
+    {
+
+    'que' : 'In how many ways CSS can be written in',
+    'a': '1',
+    'b': '2',
+    'c': '3',
+    'd': '4',
+    'correct' : 'c'
+
+},
+    {
+
+    'que' : 'Which tag is used to declare internal CSS?',
+    'a': '<style>',
+    'b': '<link>',
+    'c': '<script>',
+    'd': 'None of the above',
+    'correct' : 'a'
+
+},
+    {
+
+    'que' : 'Can negative values be allowed in padding property?',
+    'a': 'Yes',
+    'b': 'No',
+    'c': 'Depends on property',
+    'd': 'None of the above',
+    'correct' : 'b'
+
+},
+    {
+
+    'que' : 'The CSS property used to specify the transparency of an element?',
+    'a': 'Opacity',
+    'b': 'Visibility',
+    'c': 'Filter',
+    'd': 'All of the above',
+    'correct' : 'a'
+
+},
+    {
+
+    'que' : 'What are the different types of gradient in CSS?',
+    'a': 'Linear',
+    'b': 'Conic',
+    'c': 'Radial',
+    'd': 'All of the above',
+    'correct' : 'd'
+
+},
+    {
+
+    'que' : 'CSS property used to make the text bold?',
+    'a': 'weight:bold',
+    'b': 'font:bold',
+    'c': 'font-weight:bold',
+    'd': 'style:bold',
+    'correct' : 'c'
+
+},
+    {
+
+    'que' : 'Which of the following is not type of combinator?',
+    'a': '~',
+    'b': '+',
+    'c': '>',
+    'd': '*',
+    'correct' : 'd'
+
+},
+    {
+
+    'que' : 'Which CSS proprty defines the radius of an element\'s corner?',
+    'a': 'border-corner',
+    'b': 'corner',
+    'c': 'border-radius',
+    'd': 'radius',
+    'correct' : 'c'
+
+},
+    {
+
+    'que' : 'What is the valid value of alpha parameter?',
+    'a': '0 to 125',
+    'b': '0 to 90',
+    'c': '0 to 16',
+    'd': '0.1 to 1.0',
+    'correct' : 'd'
+
+},
+    {
+
+    'que' : 'Which of the following are valid ways to represent a color in CSS?',
+    'a': 'A valid color name',
+    'b': 'RGB values',
+    'c': 'HEX values',
+    'd': 'All of the above',
+    'correct' : 'd'
+
+},
+    {
+
+    'que' : 'How do we put Javascript inside HTML',
+    'a': '<js>',
+    'b': '<scripting>',
+    'c': '<script>',
+    'd': '<javascript>',
+    'correct' : 'c'
+
+},
+    {
+
+    'que' : 'Javascript is what type of language?',
+    'a': 'Object Oriented',
+    'b': 'Object Based',
+    'c': 'procedural',
+    'd': 'None of the above',
+    'correct' : 'a'
+
+},
+    {
+
+    'que' : 'Which of the following keyword is used to define vriable declration in JS',
+    'a': 'var',
+    'b': 'let',
+    'c': 'Both',
+    'd': 'None of the above',
+    'correct' : 'c'
+
+},
+    {
+
+    'que' : 'What does NaN means?',
+    'a': 'Negative Number',
+    'b': 'Not a Number',
+    'c': 'Both',
+    'd': 'None of the above',
+    'correct' : 'b'
+
+},
+    {
+
+    'que' : 'Which event is related to keyboard',
+    'a': 'onfocus',
+    'b': 'onclick',
+    'c': 'onkeydown',
+    'd': 'onkeypress',
+    'correct' : 'd'
+
+},
+    {
+
+    'que' : 'Which of the method is used to refresh webpage',
+    'a': 'window.reload()',
+    'b': 'window.refresh()',
+    'c': 'location.reload()',
+    'd': 'location.refresh()',
+    'correct' : 'c'
+
+},
+    {
+
+
+    'que' : 'Which one is a ternery operator',
+    'a': '< ?',
+    'b': '? :',
+    'c': '> =',
+    'd': '< >',
+    'correct' : 'b'
+
+},
+    {
+
+
+    'que' : 'Which of the following is Popup box in JS',
+    'a': 'alert',
+    'b': 'prompt',
+    'c': 'confirm',
+    'd': 'All of the above',
+    'correct' : 'd'
+
+},
+    {
+
+    'que' : 'Javascript is interpreted by?',
+    'a': 'Client',
+    'b': 'Server',
+    'c': 'Object',
+    'd': 'None of the above',
+    'correct' : 'a'
+
+}
+
+];
+
+console.log(questions)
+
+
+let index = 0;
+let right = 0;
+let wrong = 0;
+let total = questions.length;
+let ques = document.getElementById('ques');
+let options = document.querySelectorAll('.options');
+
+const loadQuestion = () =>{
+    if(index===total){
+
+        return endQuiz();
+    }
+    
+    reset();
+    // laod the question
+
+    ques.innerText=`Q ${index+1})  ${questions[index].que}`;
+    options[0].nextElementSibling.innerText=questions[index].a;
+    options[1].nextElementSibling.innerText=questions[index].b;
+    options[2].nextElementSibling.innerText=questions[index].c;
+    options[3].nextElementSibling.innerText=questions[index].d;
+    // loads the respective options
+}
+
+const submitQuiz = (e) =>{
+
+
+    let data = questions[index];
+   let ans = getAnswer();
+   if(ans===data.correct){
+    right ++;
+   }else{
+    wrong++;
+   }
+   index++;
+   loadQuestion();
+   return;
+}
+
+
+const getAnswer = () =>{
+    let error=document.getElementById('error');
+    let answer ;
+    options.forEach((input)=>{
+        if(input.checked){
+            answer = input.value;
+        }
+        
+    })
+    return answer;
+}
+
+const reset = () =>{
+    options.forEach((input) =>{
+        input.checked=false;
+    })
+}
+
+const endQuiz = () =>{
+
+
+
+    let box = document.getElementById('box')
+    let head = document.getElementById('head')
+    head.style.display="none";
+    box.innerHTML=`<div class="text-center">
+    <i class="fa fa-check-circle done" aria-hidden="true"></i>
+    <br><br>
+    <h2>Congratulations your quiz has been submitted successfully..!!</h2>
+    <a href="#" class="fs-5 mx-1" data-mdb-target="#score" data-mdb-toggle="collapse">View Score</a>
+    <a href="#" class="fs-5 mx-1" data-mdb-target="#ans" data-mdb-toggle="collapse">Answers</a>
+    <a href="#" class="fs-5 mx-1" onclick="location.reload()">Try again</a>
+    <div class="collapse my-3" id="score">
+        <h3>Your Score is ${right} / ${total} </h3>
+    </div>
+
+    <div class="collapse mt-3 text-start" id="ans">
+      
+<ol type="1" class="text-start">
+
+<li>Both</li>
+<li>6</li>
+<li>h6</li>
+<li>id</li>
+<li>img</li> 
+
+<li>RGB(0,0,0)</li>
+<li>All of the above</li>
+<li>ordered , unordered list</li>
+<li>.html</li>
+<li> < > </li>
+
+<li>Cascading Style Sheets</li>
+<li>3</li>
+<li> < > </li>
+<li>No</li>
+<li> Opacity </li>
+
+<li>All of the above</li>
+<li>font-weight:bold</li>
+<li> * </li>
+<li>border-radius</li>
+<li> 0.1 to 1.0 </li>
+
+<li>All of the above</li>
+<li> script  </li>
+<li>Object Oriented</li>
+<li>Both</li>
+<li> Not a Number</li>
+
+<li>onkeypress</li>
+<li>location.reload()</li>
+<li>? : </li>
+<li>All of the above</li>
+<li> Clinet</li>
+
+</ol>  
+</div>
+    
+</div>`;
+}
+
+// intial call to the function
+loadQuestion();
+
